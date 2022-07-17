3efeeeb35ddd8c14d6d16449f50a81c5dc57add7
Eliyas Hossain <eliyashossain700@gmail.com>
2022-07-17 18:48:39 +0600

    Set up completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import UitStar from "@iconscout/react-unicons-thinline/icons/uit-star";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Weather</h1>
+      <UitStar size="140" color="#61DAFB" />
     </div>
   );
 }
